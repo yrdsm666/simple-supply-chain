6c4ec2cd0a74f6ea95fe4a660c1b6ba702d65d16
f4prime <zzz136454872@163.com>
2022-03-04 17:25:31 +0800

    longer timeout

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
     baseURL: 'http://127.0.0.1:8080',
-    timeout: 5000
+    timeout: 15000,
 });
 
 service.interceptors.request.use(
